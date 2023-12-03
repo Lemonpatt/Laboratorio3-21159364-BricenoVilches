06587abb891462de8294f4cd6e3aac9be389aefe
LemonPatt <patricio.briceno@usach.cl>
2023-12-02 23:58:48 -0300

    Rf 2 y 3 referido al flujo

--- Lab-3/src/main/java/clases_21159364_BricenoVilches/Flow_21159364_BricenoVilches.java
+++ Lab-3/src/main/java/clases_21159364_BricenoVilches/Flow_21159364_BricenoVilches.java
@@ -44,20 +44,48 @@ public class Flow_21159364_BricenoVilches implements FlowInterfaz_21159364_Brice
     public List<Option_21159364_BricenoVilches> getOptions() {
         return options;
     }
-    public static Flow_21159364_BricenoVilches crearFlowConsola(Scanner input) {
+    public static Flow_21159364_BricenoVilches crearFlowConsola(Scanner input, List<Option_21159364_BricenoVilches> optionsDisponibles) {
         System.out.println("Ingrese el ID:");
         int id = input.nextInt();
         input.nextLine();
         System.out.println("Ingrese el nombre del flujo:");
         String name = input.nextLine();
         List<Option_21159364_BricenoVilches> opciones = new ArrayList<>();
-        System.out.println("Ingrese las opciones (separadas por espacio):");
-        String opcionesInput = input.nextLine();
-        String[] opcionesArray = opcionesInput.split(" ");
-        for (String opcion : opcionesArray) {
-            opciones.add(opcion);
+        System.out.println("Opciones disponibles:");
+        for (int i = 0; i < optionsDisponibles.size(); i++) {
+            System.out.println((i + 1) + ". " + optionsDisponibles.get(i).getId());
         }
+        System.out.println("Elija el número de la opcion a agregar (presione 0 para salir):");
+        int optionChoice;
+        do {
+            System.out.println("Número de opción:");
+            optionChoice = input.nextInt();
+            input.nextLine(); // Consume the newline
+
+            if (optionChoice > 0 && optionChoice <= optionsDisponibles.size()) {
+                Option_21159364_BricenoVilches opcionElegida = optionsDisponibles.get(optionChoice -1);
+                if (!optionDuplicada(optionsDisponibles, opcionElegida.getId())) {
+                    opciones.add(optionsDisponibles.get(optionChoice - 1));
+                } else {
+                    System.out.println("La Id de esta opción se encuentra repetida en el Flujo");
+                }
+            } else if (optionChoice != 0) {
+                System.out.println("Número de opción inválido. Inténtelo de nuevo.");
+            }
+        } while (optionChoice != 0);
 
         return new Flow_21159364_BricenoVilches(id, name, opciones);
     }
+    public void flowAddOption(Option_21159364_BricenoVilches option){
+        this.options.add(option);
+    }
+    private static boolean optionDuplicada(List<Option_21159364_BricenoVilches> options, int id){
+        for(Option_21159364_BricenoVilches option : options){
+            if (option.getId() == id){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
